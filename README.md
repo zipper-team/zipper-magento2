a382f2fcab5d88bad617789dceb621c476aed0a0
note072 <brunocordioli072@gmail.com>
2021-06-07 09:26:50 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# zipper-magento2
+> zipper-magento2
+
+[![Codacy Badge](https://app.codacy.com/project/badge/Grade/d0047086b0bd4df2934559f6dcf0d597)](https://www.codacy.com/gh/zipper-team/zipper-magento2/dashboard?utm_source=github.com&amp;utm_medium=referral&amp;utm_content=zipper-team/zipper-magento2&amp;utm_campaign=Badge_Grade)
+[![Code Style: Google](https://img.shields.io/badge/code%20style-google-blueviolet.svg)](https://github.com/google/gts)
+
+## Build Setup
+
+```bash
+# up docker
+$ docker-compose up -d
+
+# install dependencies
+$ yarn
+
+# insert products in magento
+$ yarn insert
+```
+
---
+++ index.js
@@ -0,0 +1,30 @@
+/* eslint-disable max-len */
+const {MagentoIntegration} = require('@sejazipper/magento2-api');
+const _ = require('lodash');
+const config = require('config');
+
+const magento = new MagentoIntegration({
+  accessToken: config.get('accessToken'),
+  accessTokenSecret: config.get('accessTokenSecret'),
+  consumerKey: config.get('consumerKey'),
+  consumerSecret: config.get('consumerSecret'),
+  url: config.get('url'),
+});
+
+const produtos = require('./resources/produtos.json');
+
+const produtosWithImage = produtos.filter((el) => el.imagens);
+console.log(`Produtos com imagem: ${produtosWithImage.length} of ${produtos.length}`);
+
+const groupedProducts = _.groupBy(produtosWithImage, 'categoria');
+const saveProductsFromCategory = (category, quantity) => {
+  console.log(`Category size: ${groupedProducts[category].length}`);
+  magento.saveZipperProducts(groupedProducts[category].slice(0, quantity));
+};
+
+console.log('Categories: ', Object.keys(groupedProducts));
+
+saveProductsFromCategory('CHUTEIRA', 10);
+saveProductsFromCategory('MEIA', 10);
+saveProductsFromCategory('CAMISETA', 10);
+saveProductsFromCategory('CALÇA', 10);
